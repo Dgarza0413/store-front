621a76154784a280a66777d6b9245a7f7dd37e63
David Garza <dgarza0413@gmail.com>
2020-08-06 19:57:38 -0500

    slider data mapped

--- client/src/pages/grid.js
+++ client/src/pages/grid.js
@@ -9,7 +9,7 @@ export const Image = styled.img`
 `
 export const Grid = styled.div`
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-columns: 1fr 1fr 1fr;
 `
 
 const GridPage = () => {
--- client/src/pages/slider.js
+++ client/src/pages/slider.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 import Slider from "react-slick";
+import styled from '@emotion/styled';
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+export const Image = styled.img`
+  width: 300px;
+  height: 300px;
+`
+export const Grid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+`
+
 const SliderPage = () => {
-    var settings = {
+    const [data, setData] = useState([])
+
+    const settings = {
         dots: true,
         infinite: true,
         speed: 500,
@@ -13,26 +27,30 @@ const SliderPage = () => {
         slidesToScroll: 1
     };
 
+    const shopifyApi = () => {
+        axios.get('http://localhost:3001/api/products')
+            .then(res => setData(res.data.products))
+            .catch(err => console.error(err))
+    }
+
+    useEffect(() => {
+        shopifyApi()
+    }, [])
+
     return (
         <Slider {...settings}>
-            <div>
-                <h3 style={{ backgroundColor: 'blue' }}>1</h3>
-            </div>
-            <div>
-                <h3>2</h3>
-            </div>
-            <div>
-                <h3>3</h3>
-            </div>
-            <div>
-                <h3>4</h3>
-            </div>
-            <div>
-                <h3>5</h3>
-            </div>
-            <div>
-                <h3>6</h3>
-            </div>
+            {data.map((e, i) => {
+                return (
+                    <React.Fragment key={i}>
+                        <Link to="/detail">
+                            <Image
+                                src={e.image.src}
+                                alt={'alt'}
+                            />
+                        </Link>
+                    </React.Fragment>
+                )
+            })}
         </Slider>
     );
 }
