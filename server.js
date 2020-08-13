f7e79f1b5e3f527589fd90e17732ffb40688b03e
David Garza <dgarza0413@gmail.com>
2020-08-13 15:40:24 -0500

    altered app use

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ if (process.env.NODE_ENV === "production") {
 
 app.use((req, res) => {
     // res.sendFile(path.join(__dirname, "client/build/index.html"));
-    res.sendFile(path.join(__dirname, "../client/public/index.html"));
+    res.sendFile(path.join(__dirname, "client/public/index.html"));
 })
 
 
