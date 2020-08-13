0424f1ce6828b79fe3fb780dcda2cc208f9e6058
David Garza <dgarza0413@gmail.com>
2020-08-13 15:35:50 -0500

    error in port variable

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const compression = require('compression');
 const cors = require('cors');
 const app = express();
 
-const PORT = 3001 || process.env.PORT;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
