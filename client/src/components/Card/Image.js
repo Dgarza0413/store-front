f73058e120057a78fee610ab410741c71a7614c1
David Garza <dgarza0413@gmail.com>
2020-10-20 15:28:35 -0500

    image height card component

--- client/src/components/Card/Image.js
+++ client/src/components/Card/Image.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Card from 'react-bootstrap/Card';
+import { Image } from 'cloudinary-react';
 
 const Image = ({ src }) => {
     return (
         <Card.Img
             variant="top"
-            className="rounded-circle"
+            style={{ height: '18rem' }}
+            className="rounded-circle "
             src={src || `https://picsum.photos/300/300?random=${Math.floor(Math.random() * 200) + 1}`} />
     )
 }
